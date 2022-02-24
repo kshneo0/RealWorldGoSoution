21b2032f6aead0481a99a42eb47b68579b5aad0f
kshneo0 <kshneo@gmail.com>
2022-02-24 09:32:54 +0900

    sec1 ch3 Using the Bytes and Strings Packages

---
+++ sec1/bytestrings/example/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/RealWorldGoSolution/sec1/bytestrings"
+
+func main() {
+	err := bytestrings.WorkWithBuffer()
+	if err != nil {
+		panic(err)
+	}
+
+	bytestrings.SearchString()
+	bytestrings.ModifyString()
+	bytestrings.StringReader()
+}
