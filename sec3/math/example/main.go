fa3e4b915f90668bf9b79f9788bfd21edc1a2bc8
kshneo0 <kshneo@gmail.com>
2022-02-25 10:15:20 +0900

    sec3 ch16 Working with Numeric Data Types using math and math/big

---
+++ sec3/math/example/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/RealWorldGoSolution/sec3/math"
+)
+
+func main() {
+	math.Examples()
+
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%v ", math.Fib(i))
+	}
+	fmt.Println()
+}
