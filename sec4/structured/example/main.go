9ad7fc0a8d0797a58448ae75ea66e73737756c12
kshneo0 <kshneo@gmail.com>
2022-02-27 13:35:41 +0900

    sec4 ch25 Structured Logging with the apex and logrus Packages

---
+++ sec4/structured/example/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/RealWorldGoSolution/sec4/structured"
+)
+
+func main() {
+	fmt.Println("Logrus:")
+	structured.Logrus()
+
+	fmt.Println()
+	fmt.Println("Apex:")
+	structured.Apex()
+}
