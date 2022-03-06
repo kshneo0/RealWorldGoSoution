d77b41a3d4c230db2e15b70fef46b82ef2fabba3
kshneo0 <kshneo@gmail.com>
2022-03-06 17:44:44 +0900

    sec12 ch59 Using the context Package

---
+++ sec12/context/example/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/RealWorldGoSolution/sec12/context"
+
+func main() {
+	context.Exec()
+}
---
+++ sec12/context/exec.go
@@ -0,0 +1,33 @@
+package context
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"golang.org/x/net/context"
+)
+
+func Exec() {
+	ctx := context.Background()
+	ctx = Setup(ctx)
+
+	rand.Seed(time.Now().UnixNano())
+
+	timeoutCtx, cancel := context.WithTimeout(ctx, (time.Duration(rand.Intn(2)) * time.Millisecond))
+	defer cancel()
+
+	deadlineCtx, cancel := context.WithDeadline(ctx, time.Now().Add(time.Duration(rand.Intn(2))*time.Millisecond))
+	defer cancel()
+
+	for {
+		select {
+		case <-timeoutCtx.Done():
+			fmt.Println(GetValue(ctx, timeoutKey))
+			return
+		case <-deadlineCtx.Done():
+			fmt.Println(GetValue(ctx, deadlineKey))
+			return
+		}
+	}
+}
